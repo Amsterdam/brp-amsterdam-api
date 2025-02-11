4070e222ce4e9198ad48f3258363015e7477ff54
Diederik van der Boor <vdboor@edoburu.nl>
2025-02-11 14:44:45 +0100

    Update CSV files for available Haal Centraal fields

--- src/config/haal_centraal/personen/update.sh
+++ src/config/haal_centraal/personen/update.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-wget https://raw.githubusercontent.com/BRP-API/Haal-Centraal-BRP-bevragen/master/features/fields-Persoon.csv
-wget https://raw.githubusercontent.com/BRP-API/Haal-Centraal-BRP-bevragen/master/features/fields-filtered-Persoon.csv
-wget https://raw.githubusercontent.com/BRP-API/Haal-Centraal-BRP-bevragen/master/features/fields-filtered-PersoonBeperkt.csv
+set -eo pipefail
+
+declare -a urls=(
+https://raw.githubusercontent.com/BRP-API/Haal-Centraal-BRP-bevragen/master/features/fields-Persoon.csv
+https://raw.githubusercontent.com/BRP-API/Haal-Centraal-BRP-bevragen/master/features/fields-filtered-Persoon.csv
+https://raw.githubusercontent.com/BRP-API/Haal-Centraal-BRP-bevragen/master/features/fields-filtered-PersoonBeperkt.csv
+)
+
+for url in "${urls[@]}"
+do
+  echo "Updating $url"
+  output="$(basename "$url")"
+  curl -f "$url" | grep -v '^pad$' > "$output.tmp"
+  mv "$output.tmp" "$output"
+done
