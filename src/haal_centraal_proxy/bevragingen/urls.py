c35f2e642c83098fa6487d3cd386ea30a066d321
Diederik van der Boor <vdboor@edoburu.nl>
2025-04-28 11:19:35 +0200

    Added views on /bevragingen/v1/ so healthchecks work

--- src/haal_centraal_proxy/bevragingen/urls.py
+++ src/haal_centraal_proxy/bevragingen/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="brp-index")),
+    path("v1/", views.IndexView.as_view(), name="brp-index"),
     path("v1/personen", views.BrpPersonenView.as_view(), name="brp-personen"),
     path("v1/bewoningen", views.BrpBewoningenView.as_view(), name="brp-bewoningen"),
     path(
--- src/haal_centraal_proxy/bevragingen/views/__init__.py
+++ src/haal_centraal_proxy/bevragingen/views/__init__.py
@@ -2,10 +2,12 @@
 
 # Split in a package for easier maintenance
 from .bewoningen import BrpBewoningenView
+from .index import IndexView
 from .personen import BrpPersonenView
 from .verblijfplaatshistorie import BrpVerblijfplaatshistorieView
 
 __all__ = (
+    "IndexView",
     "BrpPersonenView",
     "BrpBewoningenView",
     "BrpVerblijfplaatshistorieView",
