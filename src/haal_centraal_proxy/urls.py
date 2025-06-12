5833da9a9700cb28cab6469bd490726e2f6629e6
Diederik van der Boor <vdboor@edoburu.nl>
2025-06-12 14:43:46 +0200

    Move healthcheck outside public ingress, added test

--- src/haal_centraal_proxy/urls.py
+++ src/haal_centraal_proxy/urls.py
@@ -11,7 +11,10 @@ handler404 = views.not_found
 handler500 = views.server_error
 
 urlpatterns = [
+    # Routed by the public ingress:
     path("bevragingen/", include(haal_centraal_proxy.bevragingen.urls)),
+    # outside public ingress:
+    path("health/", include(haal_centraal_proxy.bevragingen.urls.health_urls)),
     path("", views.RootView.as_view()),
 ]
 
