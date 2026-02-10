0f0df2b94292766853b8414ccdba43ab9ade89f7
Jasper Jager <j.jager@amsterdam.nl>
2026-02-10 14:47:22 +0100

    Add wsdl for brp-v client

--- src/brp_amsterdam_api/bevragingen/clients/brp_v.py
+++ src/brp_amsterdam_api/bevragingen/clients/brp_v.py
@@ -1,8 +1,10 @@
 import requests
+from django.conf import settings
 from rest_framework import status
 from rest_framework.exceptions import APIException
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
+from zeep import Client, Transport
 
 from .base import BaseBrpClient
 
@@ -12,6 +14,15 @@ class BrpVAdhocServiceClient(BaseBrpClient):
     BRP V Adhoc Service Client for connection to the ad-hoc BRP service to request partner history
     since this (currently) isn't part of the RvIG BRP APIs.
     """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        transport = Transport(session=self._session)
+
+        wsdl = f"{settings.SRC_DIR}/brp_amsterdam_api/bevragingen/clients/lrd_plus.wsdl"
+        self.client = Client(wsdl=wsdl, transport=transport)
+        self.factory = self.client.type_factory("ns0")
 
     def call(self, hc_request: dict | None = None) -> requests.Response | APIException:
         # Mock response untill we have a connection
